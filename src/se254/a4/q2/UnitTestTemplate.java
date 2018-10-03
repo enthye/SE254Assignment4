da3575423aeb36ad8b33b7e0de7e5f94d711cf0f
enthye <37763349+enthye@users.noreply.github.com>
2018-10-03 17:01:10 +1300

    test template

--- src/se254/a4/q2/UnitTestTemplate.java
+++ src/se254/a4/q2/UnitTestTemplate.java
@@ -1,5 +1,7 @@
 package se254.a4.q2;
 
+import java.lang.reflect.*;
+
 public class UnitTestTemplate
 {
   protected static String nl;
@@ -12,12 +14,25 @@ public class UnitTestTemplate
   }
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = "// Your code goes here!";
+  protected final String TEXT_1 = "import static org.junit.Assert.*;" + NL + "import org.junit.Test;" + NL + "" + NL + "public class ";
+  protected final String TEXT_2 = "Test {";
+  protected final String TEXT_3 = NL + "\t@Test" + NL + "\tpublic void test";
+  protected final String TEXT_4 = "() {" + NL + "\t\tfail(\"Not yet implemented\");" + NL + "\t}\t";
+  protected final String TEXT_5 = NL + "}";
 
   public String generate(Object argument)
   {
     final StringBuffer stringBuffer = new StringBuffer();
+     Class c = (Class) argument; 
     stringBuffer.append(TEXT_1);
+    stringBuffer.append(c.getSimpleName());
+    stringBuffer.append(TEXT_2);
+     for(Method m : c.getDeclaredMethods()) { 
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append(m.getName().substring(0,1).toUpperCase() + m.getName().substring(1) );
+    stringBuffer.append(TEXT_4);
+    }
+    stringBuffer.append(TEXT_5);
     return stringBuffer.toString();
   }
 }
